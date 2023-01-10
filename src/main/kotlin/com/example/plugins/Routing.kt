9cffd867b0e8f36c462909d271b51c4ad11796a0
Jorge Eliecer Soto Ramos <36306814+Jesr2104@users.noreply.github.com>
2023-01-10 19:47:28 +0000

    TrickyWord Ktor backend - Version: 1.27

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -3,6 +3,8 @@ package com.example.plugins
 import com.example.routes.bookInfoRouting
 import com.example.routes.booksRouting
 import com.example.routes.wordsRouting
+import com.example.utilities.getStringWeb
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -10,7 +12,11 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Hello this the BackEnd-TrickyWord!")
+            call.respondText(
+                "Hello this the BackEnd-TrickyWord!" +
+                    getStringWeb(),
+                ContentType.Text.Html
+            )
         }
         bookInfoRouting()
         booksRouting()
---
+++ src/main/kotlin/com/example/utilities/StringWeb.kt
@@ -0,0 +1,66 @@
+package com.example.utilities
+
+fun getStringWeb(): String {
+    return "<h3> Books </h3>" +
+
+           "<table border=1>" +
+            "  <tr>" +
+            "    <th style='width: 200px;'>Route: </th>" +
+            "    <th style='width: 300px;'>Explanation: </th>" +
+            "  </tr>" +
+            "  <tr>" +
+            "    <td>/books</td>" +
+            "    <td>Get all the books and their words</td>" +
+            "  </tr>" +
+            "  <tr>" +
+            "    <td>/books/sort</td>" +
+            "    <td>Get all the words order by books</td>" +
+            "  </tr>" +
+            "  <tr>" +
+            "    <td>/books/{bookNumber}</td>" +
+            "    <td>Get all the word from a specific book</td>" +
+            "  </tr>" +
+            "</table>" +
+
+            "<h3> Words </h3>" +
+
+            "<table border=1>" +
+            "  <tr>" +
+            "    <th style='width: 200px;'>Route: </th>" +
+            "    <th style='width: 300px;'>Explanation: </th>" +
+            "  </tr>" +
+            "  <tr>" +
+            "    <td>/words</td>" +
+            "    <td>Get all the words from all books</td>" +
+            "  </tr>" +
+            "  <tr>" +
+            "    <td>/words/sort</td>" +
+            "    <td>Get all the words order by alphabetically</td>" +
+            "  </tr>" +
+            "  <tr>" +
+            "    <td>/word/{word}</td>" +
+            "    <td>Get a specific word</td>" +
+            "  </tr>" +
+            "</table>" +
+
+            "<h3> Information </h3>" +
+
+            "<table border=1>" +
+            "  <tr>" +
+            "    <th style='width: 200px;'>Route: </th>" +
+            "    <th style='width: 300px;'>Explanation: </th>" +
+            "  </tr>" +
+            "  <tr>" +
+            "    <td>/info/books</td>" +
+            "    <td>Get all the information of the books</td>" +
+            "  </tr>" +
+            "  <tr>" +
+            "    <td>/info/active</td>" +
+            "    <td>Get all the active books</td>" +
+            "  </tr>" +
+            "  <tr>" +
+            "    <td>/info/book/{bookNumber}</td>" +
+            "    <td>Get information of a specific book</td>" +
+            "  </tr>" +
+            "</table>"
+}
